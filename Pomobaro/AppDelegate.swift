c18a0d5db92d0d8f0d23edacbe1acaadb8c55a63
Curtis Rodgers <rodgersbc@gmail.com>
2018-03-23 22:42:59 -0700

    Completed event monitoring service.  Added context menu handling and "About Us" section.

--- Pomobaro/AppDelegate.swift
+++ Pomobaro/AppDelegate.swift
@@ -11,21 +11,52 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    // Main components
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-    let popover = NSPopover()
+    let popover:NSPopover = NSPopover()
+    var eventMonitor: EventMonitor?
+    let menu: NSMenu = NSMenu()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create taskbar image and click action
         if let button = statusItem.button {
             button.image = NSImage(named: NSImage.Name("TaskBarImage"))
-            button.action = #selector(togglePopover(_:))
+            button.action = #selector(statusBarButtonClicked(_:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
         
+        // Create context menu
+        constructMenu()
+        
+        // Assign view controller to popup content
         popover.contentViewController = TimerViewController.freshController()
+        
+        // Setup event monitoring
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            if let strongSelf = self, strongSelf.popover.isShown {
+                strongSelf.closePopover(sender: event)
+            }
+        }
     }
     
-    // Taskbar button clicked
-    @objc func togglePopover(_ sender: Any?) {
+    // Status bar button clicked
+    @objc func statusBarButtonClicked(_ sender: NSStatusBarButton) {
+        if let event = NSApp.currentEvent {
+            // Open context menu
+            if event.type == NSEvent.EventType.rightMouseUp {
+                closePopover(sender: nil)
+                statusItem.menu = menu
+                statusItem.popUpMenu(menu)
+                statusItem.menu = nil
+            } else {
+                // Toggle popover view
+                togglePopover(nil)
+            }
+        }
+    }
+    
+    // Toggle popover view
+    func togglePopover(_ sender: Any?) {
         if popover.isShown {
             closePopover(sender: sender)
         } else {
@@ -37,18 +68,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func showPopover(sender: Any?) {
         if let button = statusItem.button {
             popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            eventMonitor?.start()
         }
     }
     
     // Close popover
     func closePopover(sender: Any?) {
         popover.performClose(sender)
+        eventMonitor?.stop()
     }
     
-    @objc func printQuote(_ sender: Any?) {
-        let quote = "Great quote here.."
-        let author = "Curtis"
-        print("\(quote) by \(author)")
+    // Open about page
+    @objc func openAbout(_ sender: Any?) {
+        if let url = URL(string: "https://curtisrodgers.com/"), NSWorkspace.shared.open(url) {
+            
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -56,13 +90,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     // Create context menu
-    func constructMenu() -> Void {
-        let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
+    func constructMenu() {
+        menu.addItem(NSMenuItem(title: "About Us", action: #selector(AppDelegate.openAbout(_:)), keyEquivalent: "A"))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit Pomobaro", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        // TODO: Add 'About' that links to site
-        statusItem.menu = menu
     }
 }
 
