1cd20a65ee0746f1ccd029b79faf031381c28957
Curtis Rodgers <rodgersbc@gmail.com>
2018-04-21 18:37:04 -0700

    Started circle progress animation functionality.

--- Pomobaro/Views/TimerViewController.swift
+++ Pomobaro/Views/TimerViewController.swift
@@ -17,6 +17,8 @@ class TimerViewController: NSViewController {
     var resumeTapped = false;
     let pomodoroInstance: PomodoroTimer = PomodoroTimer()
     
+    // Animation
+    let shapeLayer = CAShapeLayer()
     
     // Outlets
     @IBOutlet weak var timerLabel: NSTextField!
@@ -89,8 +91,56 @@ class TimerViewController: NSViewController {
         
         currentSeconds = pomodoroInstance.getCurrentIntervalTime()
         timerLabel.stringValue = getTimeString(time: TimeInterval(currentSeconds))
+        
+        // ANIMATION
+        let trackLayer = CAShapeLayer()
+        let circularPath = NSBezierPath()
+        circularPath.appendArc(withCenter: .zero, radius: 125, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = NSColor.lightGray.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.fillColor = NSColor.clear.cgColor
+        trackLayer.lineCap = kCALineCapRound
+        
+        // TODO: SET THIS TO CENTER OF VIEW
+        trackLayer.position = CGPoint(x: 175, y: 200)
+        
+        view.layer?.addSublayer(trackLayer)
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = NSColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.fillColor = NSColor.clear.cgColor
+        shapeLayer.lineCap = kCALineCapRound
+        shapeLayer.position = CGPoint(x: 175, y: 200)
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        shapeLayer.strokeEnd = 0
+        view.layer?.addSublayer(shapeLayer)
+        
+        
+        
     }
     
+}
+
+extension NSBezierPath {
+    public var cgPath: CGPath {
+        let path = CGMutablePath()
+        var points = [CGPoint](repeating: .zero, count: 3)
+        for i in 0 ..< self.elementCount {
+            let type = self.element(at: i, associatedPoints: &points)
+            switch type {
+            case .moveToBezierPathElement:
+                path.move(to: points[0])
+            case .lineToBezierPathElement:
+                path.addLine(to: points[0])
+            case .curveToBezierPathElement:
+                path.addCurve(to: points[2], control1: points[0], control2: points[1])
+            case .closePathBezierPathElement:
+                path.closeSubpath()
+            }
+        }
+        return path
+    }
 }
 
 // Create view controller for taskbar application
